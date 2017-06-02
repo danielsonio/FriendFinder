736c030971b7b7c5ef8a96cedf3346ce49b51ee4
Daniel Walden <Ariel@Daniels-MacBook-Pro.local>
2017-06-02 12:36:27 -0500

    added routing and friendlist data

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+
+
+var friendArray = [
+  {
+    name: "Louie",
+    photo: "http://s2.dmcdn.net/-Uo5.jpg",
+    questionData: [4,2,3,1,5,1,2,3,4,5]
+  },
+  {
+    name: "Sketchy AF",
+    photo: "https://i.ytimg.com/vi/4VY4b_e0QEs/hqdefault.jpg",
+    questionData: [2,4,1,1,1,2,2,2,2,2]
+  },
+  {
+    name: "Carrie",
+    photo: "http://i.ytimg.com/vi/3zgFRFom6uA/0.jpg",
+    questionData: [3,3,3,3,3,3,3,3,3,3]
+  },
+  {
+    name: "Thor",
+    photo: "http://1.bp.blogspot.com/_2yGVHa4594A/SrkXjXKEXqI/AAAAAAAAAlw/oqZuL6hSIbA/s400/Picture+6.png",
+    questionData: [1,1,1,1,1,1,1,1,1,1]
+  },
+  {
+    name: "Bea",
+    photo: "http://static.thesuperficial.com/uploads/2009/04/0426_bea_arthur_rip_00.jpg",
+    questionData: [2,2,1,2,1,3,4,1,1,2]
+  },
+  {
+    name: "Sophia",
+    photo: "http://cdn.crownmediadev.com/dims4/default/38d27b9/2147483647/crop/590x367%2B4%2B209/resize/514x320%5E/quality/90/?url=http%3A%2F%2Fcdn.crownmediadev.com%2Ff0%2F59%2F9f7d28eb4440b79fa7eb4b76e1e0%2Fthegoldengirls-0005-group.jpg",
+    questionData: [5,5,5,5,5,5,5,5,5,5]
+  }
+]
+
+module.exports = friendArray;
